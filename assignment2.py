0d62c3162b2ac85304a8b11304a358f5ad8858f4
Taytatat <157549258+Taytatat@users.noreply.github.com>
2025-03-24 18:55:07 -0500

    Add files via upload

--- assignment2.py
+++ assignment2.py
@@ -16,13 +16,17 @@ data_meal = pd.read_csv("https://github.com/dustywhite7/Econ8310/raw/master/Assi
 data_pred = pd.read_csv(" https://github.com/dustywhite7/Econ8310/raw/master/AssignmentData/assignment3test.csv")
 
 
+
 #meal is the DEPENDENT VARIABLE
 
 # As usual, Patsy makes data prep easier, use ptsy to make and x and y matrix
 #<-this is how to do it with matrices: Y, X = pt.dmatrices("meal ~ -1 + Iced + Starbucks_DS_Vanilla  + Tea_Half_and_Half + Nuts_Cashews", data=data_meal) #-1 means no interncept term, meal is the depedent variable (y), everything else are our independent variables (x)
 
+
+#probably have to remove the columns wuth ID and datetime in them? Since they arent applicable
 Y = data_meal['meal'] #this meakes it so that the Depenedt variable (y) meal
-X = data_meal.drop('meal', axis =1) #makes (x) the dependent variable everything but meal
+X = data_meal.drop(['meal', 'id', 'DateTime'], axis =1) #makes (x) the dependent variable everything but meal which is Y, also drops DateTime and Id because I dont think those are there to help predicy anything but just help find whatever
+
 
 #remember that capital X, Y are different from lowercase x, y (dont want them to get overwritten)
 #x and y are training datat
@@ -35,7 +39,9 @@ x, xt, y, yt = train_test_split(X, Y,  test_size=0.33, random_state=42) #train t
 #so column names in list
 column_names = data_meal.columns.tolist()
 
-print(column_names)
+#print(column_names)
+
+print(X)
 
 #now that we have the training and testing data we can build our model
 
